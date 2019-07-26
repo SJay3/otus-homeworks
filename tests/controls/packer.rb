7a9eb3ce95aef50dc4a00374a496a714b96585fd
LANIT\DUsachev <dusachev@lanit.ru>
2019-07-26 12:17:06 +0300

    add test to packer

---
+++ tests/controls/packer.rb
@@ -0,0 +1,16 @@
+#packer validation
+
+title 'packer validation'
+
+control 'packer' do
+	impact 1
+	title 'Run packer validate'
+
+	command('find packer/ -name *.json').stdout.split.each do |fname|
+		describe command('packer validate -var-file=packer/variables.json.example 'fname) do
+			its('stdout') { should eq "Template validated successfully.\n" }
+    		its('stderr') { should eq '' }
+    		its('exit_status') { should eq 0 }
+		end
+	end
+end
