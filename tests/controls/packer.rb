34f69efcc6dd8b654d2eb5a7cb89784043795e20
LANIT\DUsachev <dusachev@lanit.ru>
2019-07-26 17:26:15 +0300

    fix packer tests

--- tests/controls/packer.rb
+++ tests/controls/packer.rb
@@ -6,8 +6,8 @@ control 'packer' do
   impact 1
   title 'Run packer validate'
 
-  command('cd packer && find -name "*.json"').stdout.split.each do |fname|
-    describe command("cd packer && packer validate -var-file=variables.json.example #{fname}") do
+  command('find packer/ -name "*.json"').stdout.split.each do |fname|
+    describe command("packer validate -var-file=variables.json.example #{fname}") do
       its('stdout') { should eq "Template validated successfully.\n" }
       its('stderr') { should eq '' }
       its('exit_status') { should eq 0 }
